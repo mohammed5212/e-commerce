863552922a82cad877929ab74fafe7ca86caaa12
mohammed5212 <mohammedchpktr@gmail.com>
2025-09-15 04:06:34 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
-const express = require("express"); 
-const dotenv = require("dotenv"); 
-const cors = require("cors"); 
+const express = require("express"); const dotenv = require("dotenv"); 
+const cors = require("cors"); dotenv.config(); 
 const orderRoutes = require("./routers/orderRoutes");
+const userRoutes = require("./routers/userRoutes");
 const app = express(); 
-dotenv.config(); 
 // Middleware 
  app.use(cors()); 
  app.use(express.json()); // 
 // Routes
   app.use("/api/auth", require("./routers/authRoutes"));
+  app.use("/api/users", userRoutes); 
   app.use("/api/orders", orderRoutes); 
 // // Example root
   app.get("/", (req, res) => {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,15 +21,16 @@ const getUserById = async(req,res)=>{
     if (req.user.role !=="admin" && req.user.id !==id){
          return res.status(403).json({ message: "Not authorized to view this user" });
     }
-    const user = await User.findById(id).select("password")
+    const user = await User.findById(id).select("-password")
     if(!user) return res.status(404).json({ message: "User not found" });
-
+    
+      res.status(200).json({ user });
    }catch (error) {
     res.status(500).json({ message: error.message });
   }
 }
 //update user (admin/user)
-const updateUser = async(req,res)=>{
+ const updateUser = async(req,res)=>{
     try{   
         const {id}=req.params
     if(req.user.role !=="admin" &&req.user.id !==id){
@@ -43,7 +44,7 @@ const updateUser = async(req,res)=>{
         const salt= await bcrypt.genSalt(10)
         updateFields.password =await bcrypt.hash(password, salt)
     }
-    const updateUser =await User.findByIdAndUpdate(id , updateFields ,{new:true,}).select("-password")
+    const updateUser =await User.findByIdAndUpdate(id , {$set:updateFields },{new:true,runValidators:true}).select("-password")
     if(!updateUser)return res.status(404).json({message:"User not found"})
 
         res.json({message:"User updated successfully",user:updateUser})
@@ -94,7 +95,7 @@ const updateProfile =async(req,res)=>{
     }
     const updatedUser =await User.findByIdAndUpdate(req.user.id,updateFields,{new:true}).select("-password")
 
-    res.json({message:"Profile updated successfully"}, updatedUser)
+    res.json({message:"Profile updated successfully",user: updatedUser})
 }catch (error) {
     res.status(500).json({ message: error.message });
   }
