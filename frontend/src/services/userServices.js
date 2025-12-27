718d8017165a96247843c523ddaef8ea829e38f7
mohammed5212 <mohammedchpktr@gmail.com>
2025-12-27 19:30:48 +0530

    mdfd cartpage ,user service

--- frontend/src/services/userServices.js
+++ frontend/src/services/userServices.js
@@ -44,18 +44,3 @@ export const registerUser = (userData) =>
 
 
 
-// import  {axiosInstance}  from "../axios/axiosInstance";
-
-// export const listProducts=() => {
-//     return axiosInstance.get('/products/listproducts');
-// };
-// export const registerUser = (userData) => {
-//     return axiosInstance.post('/user/register', userData);
-// };
-
-// export const loginUser = (credentials) => {
-//     return axiosInstance.post('/user/login', credentials);
-// }
-// export const loginAdmin= (credentials) => {
-//     return axiosInstance.post('/admin/login', credentials);
-// }
